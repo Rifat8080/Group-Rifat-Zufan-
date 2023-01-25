045a0232e6200f33071093cada3309016497be19
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-01-25 13:29:58 +0600

    Update trials.js

--- trials.js
+++ trials.js
@@ -1,1 +1,17 @@
+"use strict"; // Turning on strict mode
 
+//Md Mahadi Hasan Rifat
+
+//Bill Division Coding Challange Javascript
+
+const bonAppetit = function (bill, k, b) {
+  let solution = "";
+  bill.splice(k, 1);
+  const costPerPerson = bill.reduce((pre, post) => pre + post, 0) / 2;
+  if (costPerPerson == b) {
+    solution = "Bon Appetit";
+  } else {
+    solution = b - costPerPerson;
+  }
+  console.log(solution);
+};
